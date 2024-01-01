cae34130a949c10d5b5be0fd52bccffab6d287c4
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2024-01-01 14:44:22 -0500

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,31 +1,31 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-# my_url = 'https://www.canadacomputers.com/index.php?cPath=7_4527&sf=:&co=&mfr=&pr=1000-1499.99' # confirmed you can scrape canada computers
-# my_url = 'https://www.bestbuy.ca/en-ca/search?search=desktop'
+my_url = 'https://www.canadacomputers.com/index.php?cPath=7_4527&sf=:&co=&mfr=&pr=1000-1499.99' # confirmed you can scrape canada computers
+my_url = 'https://www.bestbuy.ca/en-ca/search?search=desktop'
 
-# uClient = uReq(my_url) # returns raw HTML of URL
+uClient = uReq(my_url) # returns raw HTML of URL
 
-# page_html = uClient.read()
+page_html = uClient.read()
 
-# uClient.close()
+uClient.close()
 
-# page_soup = soup(page_html, "html.parser") # parses html
+page_soup = soup(page_html, "html.parser") # parses html
 
-# # products = page_soup.findAll("article", {"class":"product_pod"}) # Holds all the products on the page in a list
-# title = page_soup.head.title
-# print(title)
+# products = page_soup.findAll("article", {"class":"product_pod"}) # Holds all the products on the page in a list
+title = page_soup.head.title
+print(title)
 
-# filename = 'books.csv'
-# f = open(filename, "w")
-# headers = "title, price\n"
-# f.write(headers)
+filename = 'books.csv'
+f = open(filename, "w")
+headers = "title, price\n"
+f.write(headers)
 
-# for product in products:
-#     title = product.h3.a["title"] # grabs the title of the book
-#     price = product.select('div')[1].p.get_text() # grabs price
+for product in products:
+    title = product.h3.a["title"] # grabs the title of the book
+    price = product.select('div')[1].p.get_text() # grabs price
 
-#     f.write(title.replace(",", "|") + "," + price.replace(",", "|") + "\n") # replace , with anything to avoid csv error
+    f.write(title.replace(",", "|") + "," + price.replace(",", "|") + "\n") # replace , with anything to avoid csv error
 
-# f.close
+f.close
 
